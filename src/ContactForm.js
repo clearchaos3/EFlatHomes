daa7776009f2d51e51ebb6d34dee67767f5a7a18
clearchaos3 <clearchaos3@gmail.com>
2021-03-15 14:31:59 -0500

    removed email from contact form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -7,7 +7,7 @@ export default function ContactUs() {
 
   const [userName, setUserName] = useState([]);
   const [userAddress, setUserAddress] = useState([]);
-  const [userEmail, setUserEmail] = useState([]);
+  // const [userEmail, setUserEmail] = useState([]);
   const [userPhone, setUserPhone] = useState([]);
   const [formSuccess, setFormSuccess] = useState(false);
 
@@ -21,9 +21,9 @@ export default function ContactUs() {
   const handleAddress = (e) => {
     setUserAddress(e.target.value);
   };
-  const handleEmail = (e) => {
-    setUserEmail(e.target.value);
-  };
+  // const handleEmail = (e) => {
+  //   setUserEmail(e.target.value);
+  // };
   const handlePhone = (e) => {
     setUserPhone(e.target.value);
   };
@@ -39,7 +39,7 @@ export default function ContactUs() {
 
   function sendEmail(e) {
     e.preventDefault();
-    if (userName.length > 0 && userAddress.length > 0 && userEmail.length > 0 && userPhone.length > 0){
+    if (userName.length > 0 && userAddress.length > 0 && userPhone.length > 0){
       emailjs.sendForm(serviceID, templateID, e.target, userID)
         .then((result) => {
             console.log(result.text);
@@ -67,7 +67,7 @@ export default function ContactUs() {
       <TextField name="user_name" label="Name" variant="filled" className="input" error={!userName} helperText={userName ? "" : "Please provide your name."} onChange={handleName} style={{fontFamily: "'Quicksand','sans-serif"}} /><br></br>
       <TextField name="user_address" label="Property Address" variant="filled" id="propertyAddress" className="input" error={!userAddress} helperText={userAddress ? "" : "Please provide the property address."} onChange={handleAddress} /><br></br>
       <TextField name="user_phone" label="Phone Number" variant="filled" className="input" error={!userPhone} helperText={userPhone ? "" : "Please provide your phone number."} onChange={handlePhone}/><br></br>
-      <TextField name="user_email" label="Email" variant="filled" className="input" error={!userEmail} helperText={userEmail ? "" : "Please provide your email address."} onChange={handleEmail}/><br></br>
+      {/* <TextField name="user_email" label="Email" variant="filled" className="input" error={!userEmail} helperText={userEmail ? "" : "Please provide your email address."} onChange={handleEmail}/><br></br> */}
       {/* <TextField name="user_message" label="Message (Optional)" variant="filled" className="input" multiline /><br></br><br></br> */}
       <Button variant="contained" color="secondary" style={{fontSize:25, color:'white', fontFamily:"'Quicksand','sans-serif'", fontWeight: '700'}} type="submit" value="Send" disabled={formSuccess} >{renderSuccess()}</Button><br></br>
     </form>
