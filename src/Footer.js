5864e99e48f3dcf4fdc683cdd1821ce9beb5f64d
clearchaos3 <clearchaos3@gmail.com>
2023-01-10 20:36:39 -0600

    updated address link in footer

--- src/Footer.js
+++ src/Footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
                 <Link href="tel:636-542-0404" color="secondary"><span role="img" aria-label="phone">☎️</span> (636) 542-0404</Link>
             </Button>
             <Button color="secondary" style={{ fontFamily: "'Quicksand','sans-serif'", fontWeight: 700 }}>
-                <Link href="https://goo.gl/maps/w6Rv62QLXhxv8niu7" target="_blank" color="secondary"><span role="img" aria-label="address">🏢</span> 3 Front Street, Troy MO 63379</Link>
+                <Link href="https://goo.gl/maps/5E484FDA8rNSNMc47" target="_blank" color="secondary"><span role="img" aria-label="address">🏢</span> 3 Front Street, Troy MO 63379</Link>
             </Button>
         </div>
     )
